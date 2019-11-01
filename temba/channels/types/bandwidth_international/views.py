a9ef122c2255d7702e5686253dc6c5b665af5517
ronald <istresearch@ISTs-MacBook-Pro.local>
2019-11-01 13:42:46 -0400

    PULSE-6996, PULSE-6938, checkpoint

--- temba/channels/types/bandwidth_international/views.py
+++ temba/channels/types/bandwidth_international/views.py
@@ -1,21 +1,85 @@
+import os
+import sys
 from uuid import uuid4
 
+from django import forms
+from django.core.exceptions import ValidationError
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.utils.translation import ugettext_lazy as _
+from django.utils.html import escape
+from smartmin.views import SmartFormView
+
+from temba.orgs.models import BWI_ACCOUNT_SID, BWI_APPLICATION_SID, BWI_USER_NAME, BWI_PASSWORD
 from temba.utils import analytics
-
-from temba.orgs.models import BWI_ACCOUNT_SID, BWI_ACCOUNT_TOKEN, BWI_APPLICATION_SID
-from temba.channels.types.bandwidth.views import ClaimView as BWClaimView
+from temba.utils.bandwidth import AESCipher
 from ...models import Channel
+from ...views import (
+    ALL_COUNTRIES,
+    BaseClaimNumberMixin,
+    ClaimViewMixin,
+    TWILIO_SEARCH_COUNTRIES)
+from iris_sdk import Account, Client, RestError
 
 
-class ClaimView(BWClaimView):
+class ClaimView(BaseClaimNumberMixin, SmartFormView):
     uuid = None
+
+    class Form(ClaimViewMixin.Form):
+        bwi_account_sid = forms.CharField(label="Account SID", help_text=_("Bandwidth Account ID"))
+        bwi_username = forms.CharField(label="Username", help_text=_("Bandwidth Username"))
+        bwi_password = forms.CharField(widget=forms.PasswordInput, label="Password", help_text=_("Bandwidth Password"))
+        bwi_application_sid = forms.CharField(label="Application SID", help_text=_("Bandwidth Account Application ID"))
+        # bw_phone_number = forms.CharField(label="Phone Number", help_text=_("Your Bandwidth Account Phone Number"))
+
+        def clean(self):
+
+            bwi_account_sid = self.cleaned_data.get("bwi_account_sid", None)
+            bwi_username = self.cleaned_data.get("bwi_username", None)
+            bwi_password = self.cleaned_data.get("bwi_password", None)
+            bwi_application_sid = self.cleaned_data.get("bwi_application_sid", None)
+
+            BWI_KEY = os.environ.get("BWI_KEY")
+
+            if not bwi_account_sid:  # pragma: needs cover
+                raise ValidationError(_("You must enter your Bandwidth Account ID"))
+
+            if not bwi_username:
+                raise ValidationError(_("You must enter your Bandwidth Account Username"))
+            if not bwi_password:
+                raise ValidationError(_("You must enter your Bandwidth Account Password"))
+            if not bwi_application_sid:
+                raise ValidationError(_("You must enter your Bandwidth Account's Application ID"))
+            if not BWI_KEY or len(BWI_KEY) == 0:
+                raise ValidationError(_("The environment variable BWI_KEY  must be a valid encryption key"))
+
+            try:
+                account = Account(client=Client(url="https://dashboard.bandwidth.com/api", username=bwi_username,
+                                      password=bwi_password, account_id=bwi_account_sid)).get(bwi_account_sid)
+            except RestError as error:
+                raise ValidationError(
+                    _(escape(error))
+                )
+            except Exception as error:
+                raise ValidationError(
+                    _(escape(error))
+                )
+
+            return self.cleaned_data
 
     def __init__(self, channel_type):
         super().__init__(channel_type)
         self.account = None
         self.client = None
+
+    def get_search_countries_tuple(self):
+            return TWILIO_SEARCH_COUNTRIES
+
+    def get_supported_countries_tuple(self):
+            return ALL_COUNTRIES
+
+    def get_search_url(self):
+        return reverse("channels.channel_search_numbers")
 
     def get_claim_url(self):
         return reverse("channels.types.bandwidth_international.claim")
@@ -23,7 +87,7 @@ class ClaimView(BWClaimView):
     def get_success_url(self):
             return reverse("channels.channel_read", args=[self.uuid])
 
-    form_class = BWClaimView.Form
+    form_class = Form
     submit_button_name = "Save"
     success_url = "@channels.types.bandwidth_international.claim"
     field_config = dict(bw_account_sid=dict(label=""), bw_account_token=dict(label=""))
@@ -31,38 +95,44 @@ class ClaimView(BWClaimView):
 
     def form_valid(self, form):
         bwi_account_sid = form.cleaned_data["bwi_account_sid"]
-        bwi_account_token = form.cleaned_data["bwi_account_token"]
-        bwi_account_secret = form.cleaned_data["bwi_account_secret"]
-        bwi_phone_number = form.cleaned_data["bwi_phone_number"]
+        bwi_username = form.cleaned_data["bwi_username"]
+        bwi_password = form.cleaned_data["bwi_password"]
         bwi_application_sid = form.cleaned_data["bwi_application_sid"]
-        bwi_country_sid = form.cleaned_data["bwi_country"]
 
         org = self.org
-        org.connect_bandwidth(bwi_account_sid, bwi_account_token, bwi_account_secret, bwi_phone_number,
-                              bwi_application_sid, self.request.user)
+
+        org.connect_bandwidth_international(bwi_account_sid, bwi_username, bwi_password,
+                                            bwi_application_sid, self.request.user)
         org.save()
 
-        channel = self.claim_number(self.request.user, bwi_phone_number, bwi_country_sid, Channel.ROLE_SEND + Channel.ROLE_CALL)
+        channel = self.claim_number(self.request.user, Channel.ROLE_SEND + Channel.ROLE_CALL)
 
         self.uuid = channel.uuid
         return HttpResponseRedirect(self.get_success_url())
 
-    def claim_number(self, user, phone_number, country, role):
+    def claim_number(self, user, role):
         org = user.get_org()
         self.uuid = uuid4()
         callback_domain = org.get_brand_domain()
+        bwi_key = os.environ.get("BWI_KEY")
         org_config = org.config
+
+        username = AESCipher(org_config[BWI_USER_NAME], bwi_key).encrypt()
+        password = AESCipher(org_config[BWI_PASSWORD], bwi_key).encrypt()
+
         config = {
             Channel.CONFIG_APPLICATION_SID: org_config[BWI_APPLICATION_SID],
             Channel.CONFIG_ACCOUNT_SID: org_config[BWI_ACCOUNT_SID],
-            Channel.CONFIG_AUTH_TOKEN: org_config[BWI_ACCOUNT_TOKEN],
+            Channel.CONFIG_USERNAME: username,
+            Channel.CONFIG_PASSWORD: password,
             Channel.CONFIG_CALLBACK_DOMAIN: callback_domain,
         }
 
         channel = Channel.create(
-            org, user, country, "BWI", name=org_config[BWI_ACCOUNT_SID], address=phone_number, role=role, config=config, uuid=self.uuid
+            org=org, user=user, channel_type="BWI", name=org_config[BWI_ACCOUNT_SID],
+            role=role, config=config, uuid=self.uuid, country=""
         )
 
-        analytics.track(user.username, "temba.channel_claim_bandwidth_international", properties=dict(number=phone_number))
-
+        analytics.track(user.username, "temba.channel_claim_bandwidth_international",
+                        properties=dict(account_sid=BWI_ACCOUNT_SID))
         return channel
--- temba/utils/bandwidth.py
+++ temba/utils/bandwidth.py
@@ -5,6 +5,10 @@ from bandwidth import account, messaging, voice, version
 from django.utils.encoding import force_text
 
 from temba.utils.http import HttpEvent
+from Crypto.Cipher import AES
+from Crypto.Random import new as Random
+from hashlib import sha256
+from base64 import b64encode,b64decode
 
 
 def encode_atom(atom):  # pragma: no cover
@@ -14,6 +18,27 @@ def encode_atom(atom):  # pragma: no cover
         return atom.encode("utf-8")
     else:
         raise ValueError("list elements should be an integer, " "binary, or string")
+
+
+class AESCipher:
+  def __init__(self,data,key):
+    self.block_size = 16
+    self.data = data
+    self.key = sha256(key.encode()).digest()[:32]
+    self.pad = lambda s: s + (self.block_size - len(s) % self.block_size) * chr (self.block_size - len(s) % self.block_size)
+    self.unpad = lambda s: s[:-ord(s[len(s) - 1:])]
+
+  def encrypt(self):
+    plain_text = self.pad(self.data)
+    iv = Random().read(AES.block_size)
+    cipher = AES.new(self.key,AES.MODE,iv)
+    return b64encode(iv + cipher.encrypt(plain_text.encode())).decode()
+
+  def decrypt(self):
+    cipher_text = b64decode(self.data.encode())
+    iv = cipher_text[:self.block_size]
+    cipher = AES.new(self.key,AES.MODE_OFB,iv)
+    return self.unpad(cipher.decrypt(cipher_text[self.block_size:])).decode()
 
 
 class BandwidthRestClient(account.client_module.Client):  # pragma: no cover
