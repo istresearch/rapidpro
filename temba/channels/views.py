e7ed2668c3e495c5cdd9046312fe0e36b15db01d
Marti Martinez <martinezah@gmail.com>
2019-12-09 09:48:35 -0700

    guard against referencing potentially empty SUB_DIR attribute

--- temba/channels/views.py
+++ temba/channels/views.py
@@ -1917,7 +1917,7 @@ class ChannelCRUDL(SmartCRUDL):
             context["configuration_urls"] = channel_type.get_configuration_urls(self.object)
             context["show_public_addresses"] = channel_type.show_public_addresses
 
-            if hasattr(settings, 'SUB_DIR'):
+            if hasattr(settings, 'SUB_DIR') and settings.SUB_DIR is not None:
                 context['subdir'] = settings.SUB_DIR.replace("/", "").replace("\\", "")
             return context
 
