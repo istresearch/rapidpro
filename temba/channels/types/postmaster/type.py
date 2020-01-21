c2c4942ab5af4c1da0bdabaf588aa0cd381a5c43
Joe Page <jpage4500@gmail.com>
2020-01-21 10:52:52 -0500

    - change from a single PM channel to one per type

--- temba/channels/types/postmaster/type.py
+++ temba/channels/types/postmaster/type.py
@@ -1,62 +1,27 @@
-from twilio.base.exceptions import TwilioRestException
-
 from django.utils.translation import ugettext_lazy as _
 
-from temba.channels.types.bandwidth.views import ClaimView
+from temba.channels.types.postmaster.views import ClaimView
 from temba.contacts.models import TEL_SCHEME
 
 from ...models import ChannelType
 
 
+##############################################
+# Postmaster - WhatsApp channel
+##############################################
 class PostmasterType(ChannelType):
-    """
-    An IST Postmaster channel
-    """
-
-    code = "POSTMASTER"
+    code = "PM-WA"
     category = ChannelType.Category.PHONE
 
-    courier_url = r"^bwd/(?P<uuid>[a-z0-9\-]+)/(?P<action>receive|status)$"
+    courier_url = r"^pm-wa/(?P<uuid>[a-z0-9\-]+)/(?P<action>receive|status)$"
 
-    name = "Postmaster"
+    name = "Postmaster/WhatsApp"
     icon = "icon-channel-postmaster"
     claim_blurb = _(
-        "Engage uses your Android phone to send and receive messages on your behalf over multiple channel types such as SMS or WhatsApp"
+        "Send and receive messages over WhatsApp"
     )
     claim_view = ClaimView
 
     schemes = [TEL_SCHEME]
-    max_length = 1600
-
-    def deactivate(self, channel):
-        config = channel.config
-        client = channel.org.get_bandwidth_messaging_client()
-        number_update_args = dict()
-
-        if not channel.is_delegate_sender():
-            number_update_args["sms_application_sid"] = ""
-
-        if channel.supports_ivr():
-            number_update_args["voice_application_sid"] = ""
-
-        try:
-            try:
-                number_sid = channel.bod or channel.config.get("number_sid")
-                client.api.incoming_phone_numbers.get(number_sid).update(**number_update_args)
-            except Exception:
-                if client:
-                    matching = client.api.incoming_phone_numbers.stream(phone_number=channel.address)
-                    first_match = next(matching, None)
-                    if first_match:
-                        client.api.incoming_phone_numbers.get(first_match.sid).update(**number_update_args)
-
-            if "application_sid" in config:
-                try:
-                    client.api.applications.get(sid=config["application_sid"]).delete()
-                except TwilioRestException:  # pragma: no cover
-                    pass
-
-        except TwilioRestException as e:
-            # we swallow 20003 which means our bandwidth key is no longer valid
-            if e.code != 20003:
-                raise e
+    max_length = 4096
+    attachment_support = True
