888b7b3403bc8a2d265b6a3366f390b4c2c31fb4
ronald <istresearch@ISTs-MacBook-Pro.local>
2020-01-31 09:17:08 -0500

    postmaster-channel initial commit

---
+++ temba/channels/types/postmaster/type.py
@@ -0,0 +1,37 @@
+
+from django.utils.translation import ugettext_lazy as _
+
+from temba.channels.types.postmaster.views import ClaimView
+from temba.contacts.models import TEL_SCHEME
+
+from ...models import ChannelType
+
+
+class PostmasterType(ChannelType):
+    """
+    An IST Postmaster channel
+    """
+
+    code = "PSMS"
+    category = ChannelType.Category.PHONE
+
+    courier_url = r"^psms/(?P<uuid>[a-z0-9\-]+)/(?P<action>receive|status)$"
+
+    name = "Postmaster"
+    icon = "icon-channel-postmaster"
+    claim_blurb = _(
+        """Use Postmaster compatible android devices with Pulse Engage"""
+    )
+    claim_view = ClaimView
+
+    schemes = [TEL_SCHEME]
+    max_length = 1600
+
+    def deactivate(self, channel):
+        number_update_args = dict()
+
+        if not channel.is_delegate_sender():
+            number_update_args["sms_application_sid"] = ""
+
+        if channel.supports_ivr():
+            number_update_args["voice_application_sid"] = ""
---
+++ temba/channels/types/postmaster/views.py
@@ -0,0 +1,109 @@
+from uuid import uuid4
+
+from django.core.exceptions import ValidationError
+from smartmin.views import SmartFormView
+
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from temba.utils import analytics
+from django.utils.translation import ugettext_lazy as _
+
+
+from ...models import Channel
+from ...views import (
+    ALL_COUNTRIES,
+    TWILIO_SEARCH_COUNTRIES,
+    BaseClaimNumberMixin,
+    ClaimViewMixin,
+)
+
+import temba.channels.types.postmaster.type.PostmasterType as PostmasterType
+
+
+class ClaimView(BaseClaimNumberMixin, SmartFormView):
+    uuid = None
+
+    class Form(ClaimViewMixin.Form):
+        pm_receiver_id = None
+        pm_mode = None
+
+        def clean(self):
+
+            pm_receiver_id = self.cleaned_data.get("pm_receiver_id", None)
+            pm_chat_mode = self.cleaned_data.get("pm_chat_mode", None)
+
+            if not pm_receiver_id:  # pragma: needs cover
+                raise ValidationError(_("You must provide a Receiver ID"))
+            if not pm_chat_mode:
+                raise ValidationError(_("You must select a chat mode"))
+
+            return self.cleaned_data
+
+    def __init__(self, channel_type):
+        super().__init__(channel_type)
+        self.account = None
+        self.client = None
+
+    def get_search_countries_tuple(self):
+        if self.channel_type.code == PostmasterType.code:
+            return ["US"]
+        else:
+            return TWILIO_SEARCH_COUNTRIES
+
+    def get_supported_countries_tuple(self):
+        if self.channel_type.code == PostmasterType.code:
+            return ["US"]
+        else:
+            return ALL_COUNTRIES
+
+    def get_search_url(self):
+        return reverse("channels.channel_search_numbers")
+
+    def get_claim_url(self):
+        return reverse("channels.types.postmaster.claim")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+    def get_success_url(self):
+            return reverse("channels.channel_read", args=[self.uuid])
+
+    form_class = Form
+    submit_button_name = "Save"
+    success_url = "@channels.types.postmaster.claim"
+    field_config = dict(pm_receiver_id=dict(label=""), pm_chat_mode=dict(label=""))
+    success_message = "Postmaster Channel successfully created."
+
+    def form_valid(self, form):
+        pm_receiver_id = form.cleaned_data["pm_receiver_id"]
+        pm_chat_mode = form.cleaned_data["pm_chat_mode"]
+
+        channel = self.create_channel(self.request.user, Channel.ROLE_SEND + Channel.ROLE_CALL, pm_receiver_id, pm_chat_mode)
+
+        self.uuid = channel.uuid
+        return HttpResponseRedirect(self.get_success_url())
+
+    def claim_number(self, user, phone_number, country, role):
+        analytics.track(user.username, "temba.channel_claim_postmaster",
+                        properties=dict(address=phone_number))
+        return None
+
+    def create_channel(self, user, role, pm_receiver_id, pm_chat_mode):
+        org = user.get_org()
+        self.uuid = uuid4()
+        callback_domain = org.get_brand_domain()
+        config = {
+            Channel.CONFIG_RECEIVER_ID: pm_receiver_id,
+            Channel.CONFIG_CHAT_MODE: pm_chat_mode,
+            Channel.CONFIG_CALLBACK_DOMAIN: callback_domain,
+        }
+
+        channel = Channel.create(
+            org, user, "US", PostmasterType.code, name=pm_receiver_id, address=pm_receiver_id, role=role, config=config,
+            uuid=self.uuid
+        )
+
+        analytics.track(user.username, "temba.channel_claim_postmaster", properties=dict(number=pm_receiver_id))
+
+        return channel
