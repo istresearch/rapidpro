18012a08436cadb02d34e775a4d0fa002ed462fb
Marti Martinez <martinezah@gmail.com>
2020-03-24 12:16:57 -0700

    remove queries for SyncEvents from channel list

--- temba/channels/views.py
+++ temba/channels/views.py
@@ -1609,8 +1609,7 @@ class ChannelCRUDL(SmartCRUDL):
                 org = self.request.user.get_org()
                 queryset = queryset.filter(org=org)
 
-            # order channels by role (DESC), channel (ASC) and created on date (ASC)
-            return queryset.filter(is_active=True).order_by("-role", "channel_type", "created_on")
+            return queryset.filter(is_active=True).order_by("-last_sync", "-created_on")
 
         def pre_process(self, *args, **kwargs):
             # superuser sees things as they are
@@ -1627,19 +1626,8 @@ class ChannelCRUDL(SmartCRUDL):
 
         def get_context_data(self, **kwargs):
             context = super().get_context_data(**kwargs)
-            # delayed sync event
-            sync_events = SyncEvent.objects.filter(channel__in=context['channel_list']).order_by("-created_on")
             for channel in context['channel_list']:
-                if not (channel.created_on > timezone.now() - timedelta(hours=1)):
-                    if sync_events:
-                        for sync_event in sync_events:
-                            if sync_event.channel_id == channel.id:
-                                latest_sync_event = sync_events[0]
-                                interval = timezone.now() - latest_sync_event.created_on
-                                seconds = interval.seconds + interval.days * 24 * 3600
-                                channel.last_sync = latest_sync_event
-                                if seconds > 3600:
-                                    channel.delayed_sync_event = latest_sync_event
+                channel.delayed_sync_event = channel.last_sync
             return context
 
     class Manage(OrgPermsMixin, SmartListView):
