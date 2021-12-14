125e0227026468327f6bf3d5625ebeb6f6f5e537
rsdunya <6886738+rsdunya@users.noreply.github.com>
2021-12-14 15:23:27 -0500

    Fixing cryptography lib build errors due to Rust by upgrading pip prior to installing poetry

--- poetry-install.sh
+++ poetry-install.sh
@@ -2,6 +2,7 @@
 
 python3 -m venv /venv
 source /venv/bin/activate
-python3 -m pip install -U pip poetry
+pip install --upgrade pip
+pip install poetry
 poetry install
 echo "installed pip packages"
