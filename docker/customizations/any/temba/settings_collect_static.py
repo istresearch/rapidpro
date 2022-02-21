482167c64e72cb4c149838000ee017070eb85a36
Ryan Fischbach <ryan.fischbach@twosixtech.com>
2022-02-21 16:56:46 -0500

    P4-3421: cleanup compress work.

--- docker/customizations/any/temba/settings_collect_static.py
+++ docker/customizations/any/temba/settings_collect_static.py
@@ -2,6 +2,4 @@
 from temba.settings import *  # noqa
 
 
-# Override a few settings to make storage local
-#STATIC_URL = None  # Unused
-#STATICFILES_STORAGE = 'django.core.files.storage.FileSystemStorage'
+# nothing to override, yet
--- docker/customizations/any/temba/settings_compress_static.py
+++ docker/customizations/any/temba/settings_compress_static.py
@@ -6,9 +6,8 @@ COMPRESS_ENABLED = True
 COMPRESS_OFFLINE = True
 COMPRESS_CSS_HASHING_METHOD = "content"
 
-COMPRESS_URL = STATIC_URL
+COMPRESS_URL = None
 COMPRESS_ROOT = STATIC_ROOT
-#COMPRESS_STORAGE = 'django.core.files.storage.FileSystemStorage'
 
 COMPRESS_OFFLINE_CONTEXT = dict(
     STATIC_URL=STATIC_URL, base_template="frame.html", brand=BRANDING[DEFAULT_BRAND], debug=False, testing=False
