c3fdedb188db2e4dc96e824596b61794d6725771
Ryan Fischbach <ryan.fischbach@twosixtech.com>
2022-03-01 16:25:13 -0500

    P4-3464: merge accidentally left out a required method (fixes error 500).

--- temba/contacts/views.py
+++ temba/contacts/views.py
@@ -53,7 +53,6 @@ from temba.utils.fields import (
     TembaMultipleChoiceField,
 )
 from temba.utils.models import IDSliceQuerySet, patch_queryset_count
-
 from temba.utils.views import BulkActionMixin, ComponentFormMixin, NonAtomicMixin
 
 from .models import (
@@ -1292,6 +1291,11 @@ class ContactCRUDL(SmartCRUDL):
         success_url = "uuid@contacts.contact_read"
         success_message = ""
         submit_button_name = _("Save Changes")
+
+        def get_form_kwargs(self):
+            kwargs = super().get_form_kwargs()
+            kwargs["user"] = self.request.user
+            return kwargs
 
         def get_context_data(self, **kwargs):
             context = super().get_context_data(**kwargs)
