83f9f976aacd96cd87de71e48ae9a06f36236e6d
Ryan Fischbach <ryan.fischbach@twosixtech.com>
2022-03-17 13:10:20 -0400

    ensure temba.local_settings is imported last.

--- docker/customizations/rp/temba/settings.py
+++ docker/customizations/rp/temba/settings.py
@@ -3,3 +3,9 @@ from temba.settings_engage import *  # noqa
 
 DEFAULT_BRAND = "rapidpro.io"
 BRANDING[DEFAULT_BRAND] = DEFAULT_BRAND_OBJ
+
+# in case an instance defines/overrides any previous setting
+try:
+    from temba.local_settings import *
+except ImportError:
+    pass
