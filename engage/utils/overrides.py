dd2bf6dc5565f10974ca2664e8b1b07ffaa7c485
Ryan Fischbach <ryan.fischbach@twosixtech.com>
2022-03-21 17:38:33 -0400

    P4-3545: revert contacts models code, add extra stuff via engage app.

--- engage/utils/overrides.py
+++ engage/utils/overrides.py
@@ -5,6 +5,10 @@ the form of methods put into specific classes "after initialization, but before 
 Import this file just after all the urls in the main urls.py and run its overrides.
 """
 from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
+
+import engage.contacts.models
+
 
 def _TrackUser(self):  # pragma: no cover
     """
@@ -15,6 +19,29 @@ def _TrackUser(self):  # pragma: no cover
         return True
     else:
         return False
+
+from engage.contacts.models import PM_Schemes, PM_Scheme_Labels
+from temba.contacts.models import URN as TembaURN
+TembaURN_Scheme_Choices = (
+    (TembaURN.TEL_SCHEME, _("Phone Number")),
+    (TembaURN.FACEBOOK_SCHEME, _("Facebook Identifier")),
+    (TembaURN.TWITTER_SCHEME, _("Twitter Handle")),
+    (TembaURN.TWITTERID_SCHEME, _("Twitter ID")),
+    (TembaURN.VIBER_SCHEME, _("Viber Identifier")),
+    (TembaURN.LINE_SCHEME, _("LINE Identifier")),
+    (TembaURN.TELEGRAM_SCHEME, _("Telegram Identifier")),
+    (TembaURN.EMAIL_SCHEME, _("Email Address")),
+    (TembaURN.EXTERNAL_SCHEME, _("External Identifier")),
+    (TembaURN.JIOCHAT_SCHEME, _("JioChat Identifier")),
+    (TembaURN.WECHAT_SCHEME, _("WeChat Identifier")),
+    (TembaURN.FCM_SCHEME, _("Firebase Cloud Messaging Identifier")),
+    (TembaURN.WHATSAPP_SCHEME, _("WhatsApp Identifier")),
+    (TembaURN.FRESHCHAT_SCHEME, _("Freshchat Identifier")),
+    (TembaURN.VK_SCHEME, _("VK Identifier")),
+    (TembaURN.ROCKETCHAT_SCHEME, _("RocketChat Identifier")),
+    (TembaURN.DISCORD_SCHEME, _("Discord Identifier")),
+) + tuple([ t[1] for t in sorted((lambda x: [[y[1].split(" ")[1], y] for y in x])(PM_Scheme_Labels)) ]) # Sort PM alphabetically
+
 
 def RunEngageOverrides():
     from django.contrib.auth.models import AnonymousUser, User
@@ -39,3 +66,19 @@ def RunEngageOverrides():
     from temba.orgs.views import OrgCRUDL as TembaOrgViews
     from engage.orgs.home import OrgHomeMixin as EngageOrgViews
     TembaOrgViews.Home.derive_formax_sections = EngageOrgViews.Home.derive_formax_sections
+
+    # URN is a static-only class, add in our needs
+    from temba.contacts.models import URN as TembaURN
+    for key, value in PM_Schemes.__dict__.items():
+        if not (key.startswith('__') and key.endswith('__')):
+            setattr(TembaURN, key, value)
+    TembaURN.SCHEME_CHOICES = TembaURN_Scheme_Choices
+    TembaURN.VALID_SCHEMES = {s[0] for s in TembaURN.SCHEME_CHOICES}
+    #debug
+    #for key, value in TembaURN.__dict__.items():
+    #    if not (key.startswith('__') and key.endswith('__')):
+    #        print(f"URN.{key}={value}")
+
+    # icons for schemes are kept in a separate class for some reason
+    from temba.contacts.templatetags.contacts import URN_SCHEME_ICONS
+    URN_SCHEME_ICONS.update(engage.contacts.models.PM_Scheme_Icons)
