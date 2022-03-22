a1fef18eab51049bc4443ad1a97988e15ac8e6ed
Ryan Fischbach <ryan.fischbach@twosixtech.com>
2022-03-22 10:12:01 -0400

    moved pm_schemes to pm type.

--- engage/utils/overrides.py
+++ engage/utils/overrides.py
@@ -7,9 +7,6 @@ Import this file just after all the urls in the main urls.py and run its overrid
 from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
 
-import engage.contacts.models
-
-
 def _TrackUser(self):  # pragma: no cover
     """
     Should the current user be tracked
@@ -20,9 +17,15 @@ def _TrackUser(self):  # pragma: no cover
     else:
         return False
 
-from engage.contacts.models import PM_Schemes, PM_Scheme_Labels
+from temba.channels.types.postmaster.schemes import PM_Schemes, PM_Scheme_Labels, PM_Scheme_Icons
+#from engage.utils import cap_words
 from temba.contacts.models import URN as TembaURN
-TembaURN_Scheme_Choices = (
+Processed_TembaURN_Scheme_Choices = (
+    # The lazy string translation means we cannot just cap the words in use, but instead use the Django
+    # Translation stuff: https://docs.djangoproject.com/en/4.0/topics/i18n/translation/
+    # TODO instead of overriding their string IDs, we should update the translation for en_US, but
+    #   that requires a follow-up text-bin utility which build process lacks currently.
+    #(key, cap_words(val)) for key, val in TembaURN.SCHEME_CHOICES
     (TembaURN.TEL_SCHEME, _("Phone Number")),
     (TembaURN.FACEBOOK_SCHEME, _("Facebook Identifier")),
     (TembaURN.TWITTER_SCHEME, _("Twitter Handle")),
@@ -41,7 +44,6 @@ TembaURN_Scheme_Choices = (
     (TembaURN.ROCKETCHAT_SCHEME, _("RocketChat Identifier")),
     (TembaURN.DISCORD_SCHEME, _("Discord Identifier")),
 ) + tuple([ t[1] for t in sorted((lambda x: [[y[1].split(" ")[1], y] for y in x])(PM_Scheme_Labels)) ]) # Sort PM alphabetically
-
 
 def RunEngageOverrides():
     from django.contrib.auth.models import AnonymousUser, User
@@ -72,7 +74,7 @@ def RunEngageOverrides():
     for key, value in PM_Schemes.__dict__.items():
         if not (key.startswith('__') and key.endswith('__')):
             setattr(TembaURN, key, value)
-    TembaURN.SCHEME_CHOICES = TembaURN_Scheme_Choices
+    TembaURN.SCHEME_CHOICES = Processed_TembaURN_Scheme_Choices
     TembaURN.VALID_SCHEMES = {s[0] for s in TembaURN.SCHEME_CHOICES}
     #debug
     #for key, value in TembaURN.__dict__.items():
@@ -81,4 +83,4 @@ def RunEngageOverrides():
 
     # icons for schemes are kept in a separate class for some reason
     from temba.contacts.templatetags.contacts import URN_SCHEME_ICONS
-    URN_SCHEME_ICONS.update(engage.contacts.models.PM_Scheme_Icons)
+    URN_SCHEME_ICONS.update(PM_Scheme_Icons)
---
+++ temba/channels/types/postmaster/schemes.py
@@ -0,0 +1,48 @@
+from collections import namedtuple
+
+from django.utils.translation import ugettext_lazy as _
+
+class PM_Schemes:
+    """
+    Postmaster schemes, they must all be unique and start with 'pm_'.
+    """
+    PM_ELEMENT_SCHEME       = "pm_element"
+    PM_EMAIL_SCHEME         = "pm_email"
+    PM_FACEBOOK_SCHEME      = "pm_facebook"
+    PM_FBM_SCHEME           = "pm_fbm"
+    PM_IMO_SCHEME           = "pm_imo"
+    PM_INSTAGRAM_SCHEME     = "pm_instagram"
+    PM_KAKAO_SCHEME         = "pm_kakao"
+    PM_LINE_SCHEME          = "pm_line"
+    PM_MOBYX_SCHEME         = "pm_mobyx"
+    PM_SIGNAL_SCHEME        = "pm_signal"
+    PM_TELEGRAM_SCHEME      = "pm_telegram"
+    PM_TWITTER_SCHEME       = "pm_twitter"
+    PM_VIBER_SCHEME         = "pm_viber"
+    PM_VK_SCHEME            = "pm_vk"
+    PM_WHATSAPP_SCHEME      = "pm_whatsapp"
+
+Schemes_Meta = namedtuple('Postmaster_Schemes_Meta', 'scheme label iconclass mode mode_label')
+PM_Schemes_Meta = [
+    Schemes_Meta(PM_Schemes.PM_ELEMENT_SCHEME,    _("Postmaster Element Identifier"),   "icon-element pm-icon",            "ELE",   _("Element")),
+    Schemes_Meta(PM_Schemes.PM_EMAIL_SCHEME,      _("Postmaster Email Identifier"),     "icon-envelop",                    "EMAIL", _("Email")),
+    Schemes_Meta(PM_Schemes.PM_FACEBOOK_SCHEME,   _("Postmaster Facebook Identifier"),  "icon-facebook",                   "FB",    _("Facebook")),
+    Schemes_Meta(PM_Schemes.PM_FBM_SCHEME,        _("Postmaster Messenger Identifier"), "icon-tembatoo-fbm pm-icon",       "FBM",   _("FBM")),
+    Schemes_Meta(PM_Schemes.PM_IMO_SCHEME,        _("Postmaster IMO Identifier"),       "icon-tembatoo-imo pm-icon",       "IMO",   _("IMO")),
+    Schemes_Meta(PM_Schemes.PM_INSTAGRAM_SCHEME,  _("Postmaster Instagram Identifier"), "icon-tembatoo-instagram pm-icon", "IG",    _("Instagram")),
+    Schemes_Meta(PM_Schemes.PM_KAKAO_SCHEME,      _("Postmaster Kakao Identifier"),     "icon-tembatoo-kakao pm-icon",     "KAKAO", _("KAKAO")),
+    Schemes_Meta(PM_Schemes.PM_LINE_SCHEME,       _("Postmaster Line Identifier"),      "icon-line",                       "LN",    _("LINE")),
+    Schemes_Meta(PM_Schemes.PM_MOBYX_SCHEME,      _("Postmaster Mobyx Identifier"),     "icon-tembatoo-mobyx pm-icon",     "MBX",   _("Mobyx")),
+    Schemes_Meta(PM_Schemes.PM_SIGNAL_SCHEME,     _("Postmaster Signal Identifier"),    "icon-signal pm-icon",             "SIG",   _("SIGNAL")),
+    Schemes_Meta(PM_Schemes.PM_TELEGRAM_SCHEME,   _("Postmaster Telegram Identifier"),  "icon-telegram",                   "TG",    _("Telegram")),
+    Schemes_Meta(PM_Schemes.PM_TWITTER_SCHEME,    _("Postmaster Twitter Identifier"),   "icon-twitter",                    "TWTR",  _("TWITTER")),
+    Schemes_Meta(PM_Schemes.PM_VIBER_SCHEME,      _("Postmaster Viber Identifier"),     "icon-viber pm-icon",              "VB",    _("VIBER")),
+    Schemes_Meta(PM_Schemes.PM_VK_SCHEME,         _("Postmaster VK Identifier"),        "icon-vk pm-icon",                 "VK",    _("VK")),
+    Schemes_Meta(PM_Schemes.PM_WHATSAPP_SCHEME,   _("Postmaster WhatsApp Identifier"),  "icon-whatsapp",                   "WA",    _("WhatsApp")),
+]
+
+PM_Scheme_Icons = { ico.scheme: ico.iconclass for ico in PM_Schemes_Meta }
+PM_Scheme_Labels = tuple( (x.scheme, x.label) for x in PM_Schemes_Meta )
+PM_Scheme_Default_Chats = (
+  ("SMS", _("TEL")),
+) + tuple( (x.mode, x.mode_label) for x in PM_Schemes_Meta )
