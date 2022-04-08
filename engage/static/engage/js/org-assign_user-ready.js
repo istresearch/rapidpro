1729dc6aaee63b8709c773aa6eef2e7828f50ea7
Ryan Fischbach <ryan.fischbach@twosixtech.com>
2022-04-08 09:16:54 -0400

    cleanup: removed some commented out code.

--- engage/static/engage/js/org-assign_user-ready.js
+++ engage/static/engage/js/org-assign_user-ready.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
 
-    //var theOrgPicker = $('#assign-user-org-picker');
     var theOrgPicker = $('#id_organization');
     if ( theOrgPicker ) {
         function formatOption (aOpt) {
@@ -9,23 +8,14 @@ $(document).ready(function() {
             }
             var theOptClasses = $(aOpt.element).attr('class');
             var theOpt = $(
-                '<span class="'+theOptClasses+'">' + aOpt.text + '</span><span class="status-icon '+theOptClasses+'"></span>'
+                '<span class="'+theOptClasses+'">' + aOpt.text + '</span><span class="state-icon '+theOptClasses+'"></span>'
             );
             return theOpt;
         };
         theOrgPicker.select2({
-            //dropdownParent: $('#org-area'),
             width: 'fit-content',
-            //theme: 'org-picker',
             templateResult: formatOption,
         });
-        /*
-        theOrgPicker.on('select2:select', function(e) {
-            var theOrgPK = e.params.data.id;
-            var theUrl = org_chosen_url_format.sprintf(theOrgPK);
-            posterize(theUrl);
-        });
-        */
     }
 
 });
