fd75fcc784f5e916c90020091493dcdee98a350d
Ryan Fischbach <ryan.fischbach@twosixtech.com>
2022-04-12 17:55:49 -0400

    circleci: build uses .less files from STATIC_URL properly & gzips them.

--- temba/settings_compress_static.py
+++ temba/settings_compress_static.py
@@ -1,4 +1,6 @@
 # Django settings when running compress
+from getenv import env
+import os
 
 from temba.settings import *  # noqa
 
@@ -9,8 +11,16 @@ COMPRESS_CSS_HASHING_METHOD = "content"
 COMPRESS_URL = None
 COMPRESS_ROOT = STATIC_ROOT
 
+COMPRESS_PRECOMPILERS = (
+    ("text/less", 'lessc --include-path="%s:%s" {infile} {outfile}' % (
+        os.path.join(COMPRESS_ROOT, "less"),
+        os.path.join(COMPRESS_ROOT, "engage", "less"),
+    )),
+)
+
 if env('DEV_STATIC', 'off') != 'on':
     COMPRESS_OFFLINE = True
     COMPRESS_OFFLINE_CONTEXT = dict(
         STATIC_URL=STATIC_URL, base_template="frame.html", brand=BRANDING[DEFAULT_BRAND], debug=False, testing=False
     )
+    COMPRESS_STORAGE = 'compressor.storage.GzipCompressorFileStorage'
--- temba/settings_engage.py
+++ temba/settings_engage.py
@@ -187,11 +187,6 @@ COMPRESS_OFFLINE_MANIFEST = f"manifest-{env('VERSION_CI', '1-dev')[:-4]}.json"
 #COMPRESS_OFFLINE = False
 COMPRESS_OFFLINE = COMPRESS_ENABLED and (env('DEV_STATIC', 'off') != 'on')
 
-if COMPRESS_PRECOMPILERS and len(COMPRESS_PRECOMPILERS)>0:
-    COMPRESS_PRECOMPILERS = COMPRESS_PRECOMPILERS + (
-        ("text/less", 'lessc --include-path="%s" {infile} {outfile}' % os.path.join(PROJECT_DIR, "../engage/static/engage", "less")),
-    )
-
 MAGE_AUTH_TOKEN = env('MAGE_AUTH_TOKEN', None)
 MAGE_API_URL = env('MAGE_API_URL', 'http://localhost:8026/api/v1')
 SEND_MESSAGES = env('SEND_MESSAGES', 'off') == 'on'
