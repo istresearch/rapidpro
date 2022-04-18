4daa1681e1ec75d9977939de908c79d338e2fa61
Ryan Fischbach <ryan.fischbach@twosixtech.com>
2022-04-18 17:20:01 -0400

    pygeos build with latest osgeo versions (experiment).

--- docker/geolibs.sh
+++ docker/geolibs.sh
@@ -5,16 +5,16 @@ set -e # fail on any error
 
 # Versions
 # ===================================================================
-GEOS_VERSION=3.4.3
-PROJ_VERSION=4.9.3
-PROJ_DATUMGRID_VERSION=1.5
-GDAL_VERSION=1.11.5
+GEOS_VERSION=3.10.2
+PROJ_VERSION=9.0.0
+PROJ_DATA_VERSION=1.9
+GDAL_VERSION=3.4.2
 
 
 # Install geos
 # ===================================================================
 cd /tmp
-wget http://download.osgeo.org/geos/geos-${GEOS_VERSION}.tar.bz2
+wget https://download.osgeo.org/geos/geos-${GEOS_VERSION}.tar.bz2
 tar xjf geos-${GEOS_VERSION}.tar.bz2
 cd geos-${GEOS_VERSION}
 ./configure --enable-silent-rules CFLAGS="-D__sun -D__GNUC__"  CXXFLAGS="-D__GNUC___ -D__sun"
@@ -25,11 +25,11 @@ make -s install
 # Install proj
 # ===================================================================
 cd /tmp
-wget http://download.osgeo.org/proj/proj-${PROJ_VERSION}.tar.gz
-wget http://download.osgeo.org/proj/proj-datumgrid-${PROJ_DATUMGRID_VERSION}.tar.gz
+wget https://download.osgeo.org/proj/proj-${PROJ_VERSION}.tar.gz
+wget https://download.osgeo.org/proj/proj-data-${PROJ_DATA_VERSION}.tar.gz
 tar xzf proj-${PROJ_VERSION}.tar.gz
 cd proj-${PROJ_VERSION}/nad
-tar xzf ../../proj-datumgrid-${PROJ_DATUMGRID_VERSION}.tar.gz
+tar xzf ../../proj-data-${PROJ_DATA_VERSION}.tar.gz
 cd ..
 ./configure --enable-silent-rules
 make -s
@@ -39,7 +39,7 @@ make -s install
 # Install gdal
 # ===================================================================
 cd /tmp
-wget http://download.osgeo.org/gdal/${GDAL_VERSION}/gdal-${GDAL_VERSION}.tar.gz
+wget https://download.osgeo.org/gdal/${GDAL_VERSION}/gdal-${GDAL_VERSION}.tar.gz
 tar xzf gdal-${GDAL_VERSION}.tar.gz
 cd gdal-${GDAL_VERSION}
 ./configure --enable-silent-rules --with-static-proj4=/usr/local/lib
