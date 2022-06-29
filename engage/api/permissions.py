2f48f3a2abb1b7755fc26cfec4a087cada5c3458
Ryan Fischbach <ryan.fischbach@twosixtech.com>
2022-06-29 14:24:09 -0400

    P4-3434: comment out detailed debug logging of every api request.

--- engage/api/permissions.py
+++ engage/api/permissions.py
@@ -1,5 +1,4 @@
-import logging
-from django.http import HttpRequest
+#import logging
 from rest_framework.permissions import BasePermission
 from rest_framework.request import Request
 from temba.settings import HTTP_ALLOWED_URL
@@ -9,21 +8,23 @@ class SSLorLocalTrafficPermission(BasePermission):  # pragma: no cover
     """
     Verifies that the requestor is either using HTTPS or is a local requestor.
     """
-    logger = logging.getLogger(__name__)
+    #logger = logging.getLogger(__name__)
 
     def has_permission(self, request: Request, view):
-        req: HttpRequest = request._request
-        self.logger.debug("[TRACE] ssl or local has_perm", extra={
-            'req': request,
-            'hau': HTTP_ALLOWED_URL,
-            'res': HTTP_ALLOWED_URL is None or request.is_secure() or HTTP_ALLOWED_URL == request.get_host(),
-            'is_sec': req.is_secure(),
-            'host': req.get_host(),
-            'scheme': req.scheme,
-            'hdrs': req.headers,
-            'meta': req.META,
-            'view': view,
-        })
+        # for debugging only, leave commented out otherwise:
+        # from django.http import HttpRequest
+        # req: HttpRequest = request._request
+        # self.logger.debug("[TRACE] ssl or local has_perm", extra={
+        #     'req': request,
+        #     'hau': HTTP_ALLOWED_URL,
+        #     'res': HTTP_ALLOWED_URL is None or request.is_secure() or HTTP_ALLOWED_URL == request.get_host(),
+        #     'is_sec': req.is_secure(),
+        #     'host': req.get_host(),
+        #     'scheme': req.scheme,
+        #     'hdrs': req.headers,
+        #     'meta': req.META,
+        #     'view': view,
+        # })
         return HTTP_ALLOWED_URL is None or request.is_secure() or HTTP_ALLOWED_URL == request.get_host()
     #enddef has_permission
 
