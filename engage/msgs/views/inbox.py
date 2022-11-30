9c283f37f00b5184f34a4d41bfa4ffc7b741d96a
Ryan Fischbach <ryan.fischbach@twosixtech.com>
2022-11-30 15:53:47 -0800

    P4-4185: override class super needs class type.

--- engage/msgs/views/inbox.py
+++ engage/msgs/views/inbox.py
@@ -51,7 +51,7 @@ class MsgInboxViewOverrides(ClassOverrideMixinMustBeFirst, InboxView):
 
     def get_context_data(self, **kwargs):
         context = self.getOrigClsAttr('get_context_data')(self, **kwargs) \
-                if self.getOrigClsAttr('get_context_data') else super().get_context_data(**kwargs)
+                if self.getOrigClsAttr('get_context_data') else super(self.myClassType).get_context_data(**kwargs)
         context['object_list'] = self._sanitizeMsgList(context['object_list'])
         return context
     #enddef get_context_data
