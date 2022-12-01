aac4e0f09982538f429abda2fcecfdd3cb76230e
Ryan Fischbach <ryan.fischbach@twosixtech.com>
2022-11-30 16:45:03 -0800

    P4-4185: alt super() call.

--- engage/msgs/inbox_msgfailed.py
+++ engage/msgs/inbox_msgfailed.py
@@ -13,7 +13,7 @@ class ViewInboxFailedMsgsOverrides(ClassOverrideMixinMustBeFirst, MsgCRUDL.Faile
     #enddef get_bulk_actions
 
     def get_context_data(self, **kwargs):
-        context = super(self.myClassType).origattrs.get('get_context_data')(self, **kwargs)
+        context = super(self.myClassType).get_context_data(**kwargs)
         context['object_list'] = self._sanitizeMsgList(context['object_list'])
         return context
     #enddef get_context_data
