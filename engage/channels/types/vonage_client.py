b7112ee14e6ead42da87e5895ed70394cadd273d
Ryan Fischbach <ryan.fischbach@twosixtech.com>
2022-12-04 16:49:07 -0800

    query db for channel uuids to see what numbers are in use.

--- engage/channels/types/vonage_client.py
+++ engage/channels/types/vonage_client.py
@@ -60,7 +60,6 @@ class VonageClientOverrides(ClassOverrideMixinMustBeFirst, VonageClient):
         params = {"size": size}
         if pattern:
             params["pattern"] = str(pattern).strip("+")
-        logger.debug('TRACE[get_numbers] call client.getnums')
         response = self._with_retry(self.base.get_account_numbers, params=params)
         logger.debug('TRACE[get_numbers]='+str(response))
         return response["numbers"] if int(response.get("count", 0)) else []
---
+++ engage/channels/types/vonage_views.py
@@ -0,0 +1,57 @@
+import logging
+import phonenumbers
+import re
+
+from engage.utils.class_overrides import ClassOverrideMixinMustBeFirst
+
+from temba.channels.models import Channel
+from temba.channels.types.vonage.type import VonageType
+from temba.channels.types.vonage.views import ClaimView
+
+
+logger = logging.getLogger(__name__)
+
+class ClaimViewOverrides(ClassOverrideMixinMustBeFirst, ClaimView):
+
+    def get_existing_numbers(self, org):
+        numbers = []
+        client = org.get_vonage_client()
+        if client:
+            account_numbers = client.get_numbers(size=100)
+            account_uuids = []
+            for number in account_numbers:
+                if number["type"] == "mobile-shortcode":  # pragma: needs cover
+                    phone_number = number["msisdn"]
+                else:
+                    parsed = phonenumbers.parse(number["msisdn"], number["country"])
+                    phone_number = phonenumbers.format_number(parsed, phonenumbers.PhoneNumberFormat.INTERNATIONAL)
+                #endif shortcode
+
+                # mark accounts used/unused by checking the db for uuid
+                # 'moHttpUrl': 'https://engage.dev.istresearch.com/c/nx/742c11f1-72fb-4994-8156-8848e8a63e55/receive',
+                pattern = re.compile(r"(?<=c/nx/).{8}-.{4}-.{4}-.{4}-.{12}(?=/receive)")
+                match = pattern.match(number["moHttpUrl"])
+                channel_uuid = match.string if match else None
+                account_uuids.append(channel_uuid)
+
+                numbers.append(dict(
+                    number=phone_number,
+                    country=number["country"],
+                    uuid=channel_uuid,
+                    in_use=False,
+                ))
+            #endfor numbers
+
+            # query db for "in use" numbers
+            for channel in Channel.objects.filter(
+                channel_type=VonageType.code,
+                uuid=account_uuids,
+            ):
+                idx = account_uuids.index(channel.uuid)
+                account_numbers[idx]['in_use'] = True
+            #endfor each channel found
+
+            logger.debug(' TRACE='+str(account_numbers))
+
+        #endif client
+        return numbers
