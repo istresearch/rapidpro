5b9c103b6908bfbc98d89aee2f1cfff21d7e6f34
Ryan Fischbach <ryan.fischbach@twosixtech.com>
2022-12-07 15:24:36 -0800

    P4-3030: WIP

--- engage/orgs/views/user_list.py
+++ engage/orgs/views/user_list.py
@@ -3,16 +3,43 @@ import logging
 from engage.utils.class_overrides import ClassOverrideMixinMustBeFirst
 
 from temba.orgs.views import UserCRUDL
+from temba.utils import get_anonymous_user
 
 
 logger = logging.getLogger(__name__)
 
-class OrgViewListUserMixin(ClassOverrideMixinMustBeFirst, UserCRUDL.List):
-    fields = ("username", "orgs", "date_joined")
+class OrgViewListUserOverrides(ClassOverrideMixinMustBeFirst, UserCRUDL.List):
+    fields = ("username", "orgs", "date_joined",)
     link_fields = ("username",)
     ordering = ("-date_joined",)
-    search_fields = ("username__icontains", "email__icontains")
+    search_fields = ("username__icontains", "email__icontains",)
     paginate_by = 25
+    sort_field = "username"
+    non_sort_fields = ('orgs',)
+    sort_order = None
+
+    def get_queryset(self, **kwargs):
+        """
+        override to fix sort order bug (descending uses a leading "-" which fails "if in fields" check.
+        """
+        queryset = super().get_queryset(**kwargs)
+
+        # org users see channels for their org, superuser sees all
+        if not self.request.user.is_superuser:
+            org = self.request.user.get_org()
+            queryset = queryset.filter(org=org)
+
+        theOrderByColumn = self.sort_field
+        if 'sort_on' in self.request.GET:
+            theSortField = self.request.GET.get('sort_on')
+            if theSortField in self.fields and theSortField not in self.non_sort_fields:
+                self.sort_field = theSortField
+                theSortOrder = self.request.GET.get("sort_order")
+                self.sort_order = theSortOrder if theSortOrder in ('asc', 'desc') else None
+                theSortOrderFlag = '-' if theSortOrder == 'desc' else ''
+                theOrderByColumn = "{}{}".format(theSortOrderFlag, self.sort_field)
+
+        return queryset.filter(is_active=True).order_by(theOrderByColumn, 'name').exclude(id=get_anonymous_user().id)
 
 
-#endclass OrgViewListUserMixin
+#endclass OrgViewListUserOverrides
