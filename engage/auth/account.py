d6c901d7d846731017991abd8fdde7c9b0c3aabf
Ryan Fischbach <ryan.fischbach@twosixtech.com>
2023-06-21 16:14:34 -0700

    PE-25: restore a bunch of the monkey patching of User class.

--- engage/auth/account.py
+++ engage/auth/account.py
@@ -19,6 +19,35 @@ class AuthUserOverrides(ClassOverrideMixinMustBeFirst, AuthUser):
     # default optional property to False so it exists.
     using_token = False
 
+    def __str__(self):
+        return self.name or self.username
+
+    @classmethod
+    def create(cls, email: str, first_name: str, last_name: str, password: str, language: str = None):
+        obj = cls.objects.create_user(
+            username=email, email=email, first_name=first_name, last_name=last_name, password=password
+        )
+        if language:
+            obj.settings.language = language
+            obj.settings.save(update_fields=("language",))
+        return obj
+
+    @property
+    def name(self) -> str:
+        return self.get_full_name()
+
+    @cached_property
+    def is_alpha(self) -> bool:
+        return self.groups.filter(name="Alpha").exists()
+
+    @cached_property
+    def is_beta(self) -> bool:
+        return self.groups.filter(name="Beta").exists()
+
+    @cached_property
+    def is_support(self) -> bool:
+        return self.groups.filter(name="Customer Support").exists()
+
     @cached_property
     def settings(self):
         assert self.is_authenticated, "can't fetch user settings for anonymous users"
@@ -30,6 +59,16 @@ class AuthUserOverrides(ClassOverrideMixinMustBeFirst, AuthUser):
         from temba.api.models import get_or_create_api_token
 
         return get_or_create_api_token(self)
+
+    def as_engine_ref(self) -> dict:
+        return {"email": self.email, "name": self.name}
+
+    def record_auth(self):
+        """
+        Records that this user authenticated
+        """
+        self.settings.last_auth_on = timezone.now()
+        self.settings.save(update_fields=("last_auth_on",))
 
     def has_org_perm(self, org, permission: str) -> bool:
         """
@@ -115,6 +154,23 @@ class AuthUserOverrides(ClassOverrideMixinMustBeFirst, AuthUser):
     def set_org(self, org):
         self._org = org
     #enddef set_org
+
+    def get_owned_orgs(self, *, brand=None):
+        """
+        Gets the orgs in the given brands where this user is the only user.
+        """
+        owned_orgs = []
+        for org in self.get_orgs(brands=[brand] if brand else None):
+            if not org.users.exclude(id=self.id).exists():
+                owned_orgs.append(org)
+        return owned_orgs
+
+    def set_team(self, team):
+        """
+        Sets the ticketing team for this user
+        """
+        self.settings.team = team
+        self.settings.save(update_fields=("team",))
 
 #endclass AuthUserOverrides
 
