88caf160ca366327f70e4dabbd23c628604b2243
Ryan Fischbach <ryan.fischbach@twosixtech.com>
2023-06-30 19:09:27 -0700

    PE-112: auto-switch org if contact is in another org user has access to.

--- engage/contacts/views.py
+++ engage/contacts/views.py
@@ -1,4 +1,8 @@
+from django.core.handlers.wsgi import WSGIRequest
+from django.http import Http404
+
 from engage.utils.class_overrides import ClassOverrideMixinMustBeFirst
+from engage.utils.middleware import RedirectTo
 
 from temba.contacts.views import ContactCRUDL
 
@@ -19,3 +23,32 @@ class ContactListOverrides(ClassOverrideMixinMustBeFirst, ContactCRUDL.List):
     #enddef get_gear_links
 
 #endclass ContactListOverrides
+
+class ContactReadOverrides(ClassOverrideMixinMustBeFirst, ContactCRUDL.Read):
+
+    def has_org_perm(self, permission):
+        obj_org = self.get_object_org()
+        if obj_org:
+            return self.get_user().has_org_perm(obj_org, permission)
+        else:
+            return self.getOrigClsAttr('has_org_perm')(self, permission)
+        #endif
+    #enddef
+
+    def has_permission(self, request: WSGIRequest, *args, **kwargs):
+        user = self.get_user()
+        # if user has permission to the org this contact resides, just switch the org for them
+        obj_org = self.get_object_org()
+        if user.has_org_perm(obj_org, self.permission):
+            if obj_org.pk != user.get_org().pk:
+                user.set_org(obj_org)
+                request.session["org_id"] = obj_org.pk
+                raise RedirectTo(request.build_absolute_uri())
+            #endif
+            return True
+        else:
+            return False
+        #endif
+    #enddef
+
+#endclass ContactReadOverrides
