a83c9b602bb4679ed18090d74471fce228a28e5e
Ryan Fischbach <ryan.fischbach@istresearch.com>
2023-08-28 18:05:42 -0700

    utils: class_overrides: add MonkeyPatch class as less complex.

--- engage/utils/class_overrides.py
+++ engage/utils/class_overrides.py
@@ -1,3 +1,4 @@
+import inspect
 import logging
 from typing import Any, Callable
 
@@ -41,14 +42,19 @@ def ignoreDjangoModelFormAttrs(aDjangoModelFormsClass: type[forms.ModelForm]):
 #enddef ignoreDjangoModelFormAttrs
 
 class ClassOverrideMixinMustBeFirst:
-    override_ignore: list  #only define if your overridden obj contains class vars like Django Models.
-    myClassType: type[Any]
-    origattrs: dict
-    on_apply_overrides: Callable  #only define if you have merges rather than overrides.
+    override_ignore: list  # only define if your overridden obj contains class vars like Django Models.
+    _super_class: type[Any]
+    _super_attrs: dict
+    on_apply_overrides: Callable  # only define if you have merges rather than overrides.
+
+    @classmethod
+    def getSuperClass(cls):
+        return cls._super_class
+    #enddef get_super_class
 
     @classmethod
     def getOrigClsAttr(cls, attr_name: str):
-        return cls.origattrs.get(attr_name)
+        return cls._super_attrs.get(attr_name)
     #enddef getOrigAttr
 
     @staticmethod
@@ -60,20 +66,23 @@ class ClassOverrideMixinMustBeFirst:
 
     @classmethod
     def setClassOverrides(cls) -> None:
-        ignore_attrs = getattr(cls, 'override_ignore', ()) + ('on_apply_overrides',)
+        ignore_attrs = getattr(cls, 'override_ignore', ()) + ('override_ignore', 'on_apply_overrides',)
         parents = cls.__bases__
         under_cls = parents[-1]
-        under_cls.myClassType = under_cls
-        under_cls.origattrs = {}
+        under_cls._super_class = under_cls
+        under_cls._super_attrs = {}
         under_cls.getOrigClsAttr = cls.getOrigClsAttr
         # stuff all mixins into temba_cls, too
-        class_list = parents[1:-1] + (cls,)
+        class_list = parents[1:-1] if len(parents) > 2 else () + (cls,)
+        logger.debug(f"bases= {str(class_list)}")
         for a_class in class_list:
-            for name in a_class.__dict__:
-                if not name.startswith("__") and name != 'override_ignore' and name not in ignore_attrs:
+            logger.debug(f"apply overrides from {str(a_class)}")
+            override_members = a_class.__dict__
+            for name in override_members:
+                if not name.startswith("__") and name not in ignore_attrs:
                     logger.debug(f"override: set attr {str(under_cls)}.{name} to {getattr(a_class, name)}")
                     orig_attr = getattr(under_cls, name, None)
-                    under_cls.origattrs.update({name: orig_attr})
+                    under_cls._super_attrs.update({name: orig_attr})
                     # provide overridden method a "super_" prefix so that we can easily call it
                     # more-or-less required to use this if overridden method is called by child classes, too.
                     if callable(orig_attr):
@@ -90,3 +99,49 @@ class ClassOverrideMixinMustBeFirst:
     #enddef setClassOverrides
 
 #endclass ClassOverrideMixinMustBeFirst
+
+class MonkeyPatcher:
+    _patch_class: type[Any]
+    _patch_attrs: dict
+    on_apply_overrides: Callable  # only define if you have merges rather than overrides.
+
+    @classmethod
+    def getOrigClsAttr(cls, attr_name: str):
+        return cls._patch_attrs.get(attr_name)
+    #enddef getOrigAttr
+
+    @classmethod
+    def setClassOverrides(cls) -> None:
+        ignore_attrs = ('_patch_class', '_patch_attrs', 'on_apply_overrides', 'getOrigClsAttr', 'setClassOverrides',)
+        parents = cls.__bases__
+        under_cls = cls._patch_class
+        under_cls_members = dict(inspect.getmembers(under_cls))
+        # stuff all mixins into temba_cls, too
+        class_list = parents
+        logger.debug(f"bases= {str(class_list)}")
+        for a_class in class_list:
+            logger.debug(f"apply overrides from {str(a_class)}")
+            override_members = dict(inspect.getmembers(a_class))
+            for name, memloc in override_members.items():
+                if not name.startswith("__") and name not in ignore_attrs and (name not in under_cls_members or memloc != under_cls_members[name]):
+                    logger.debug(f"override: set attr {str(under_cls)}.{name} to {getattr(a_class, name)}", extra={
+                        'mem_name': name, 'mem_loc': memloc, 'mem_cls': under_cls_members[name] if name in under_cls_members else 'N/A',
+                    })
+                    orig_attr = getattr(under_cls, name, None)
+                    cls._patch_attrs.update({name: orig_attr})
+                    # provide overridden method a "super_" prefix so that we can easily call it
+                    # more-or-less required to use this if overridden method is called by child classes, too.
+                    if callable(orig_attr):
+                        setattr(cls, f"super_{name}", orig_attr)
+                    #endif
+                    setattr(under_cls, name, getattr(a_class, name))
+                #endif
+            #endfor each class member
+        #endfor each parent
+        if getattr(cls, 'on_apply_overrides', None) and callable(getattr(cls, 'on_apply_overrides')):
+            logger.debug(f"calling {str(cls)}.on_apply_overrides({under_cls})")
+            cls.on_apply_overrides(under_cls)
+        #endif
+    #enddef setClassOverrides
+
+#endclass MonkeyPatcher
--- engage/utils/export.py
+++ engage/utils/export.py
@@ -4,18 +4,34 @@ from django.conf import settings
 from django.utils import timezone
 
 from engage.utils.class_overrides import ClassOverrideMixinMustBeFirst, ignoreDjangoModelAttrs
+from engage.utils.class_overrides import MonkeyPatcher
 
 from temba.utils.export import BaseExportTask
 
-class BaseExportTaskOverrides(ClassOverrideMixinMustBeFirst, BaseExportTask):
-    override_ignore = ignoreDjangoModelAttrs(BaseExportTask)
+# class BaseExportTaskOverrides(ClassOverrideMixinMustBeFirst, BaseExportTask):
+#     override_ignore = ignoreDjangoModelAttrs(BaseExportTask)
+#
+#     # we do not want Django to perform any magic inheritance
+#     class Meta:
+#         abstract = True
+#
+#     @classmethod
+#     def get_recent_unfinished(cls, org):
+#         """
+#         Checks for unfinished exports created recently for this org, and returns the most recent
+#         """
+#         recent_hours = settings.EXPORT_TASK_CHECK_HOURS if hasattr(settings, 'EXPORT_TASK_CHECK_HOURS') else 4
+#         recent_past = timezone.now() - timedelta(hours=recent_hours)
+#         return cls.getSuperClass().get_unfinished().filter(org=org, created_on__gt=recent_past).order_by("created_on").last()
+#     #enddef get_recent_unfinished
+#
+# #endclass BaseExportTaskOverrides
 
-    # we do not want Django to perform any magic inheritance
-    class Meta:
-        abstract = True
+class BaseExportTaskOverrides(MonkeyPatcher):
+    _patch_class: BaseExportTask
 
     @classmethod
-    def get_recent_unfinished(cls, org):
+    def get_recent_unfinished(cls: type(BaseExportTask), org):
         """
         Checks for unfinished exports created recently for this org, and returns the most recent
         """
