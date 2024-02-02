4854045e702d1962090cf3e1b3075bcc2e2adf4d
Ryan Fischbach <ryan.fischbach@istresearch.com>
2024-02-01 18:35:52 -0800

    spinner: protect against null.

--- engage/static/engage/js/frame-ready.js
+++ engage/static/engage/js/frame-ready.js
@@ -43,7 +43,10 @@ $(document).ready(function() {
     for (let ls = document.links, numLinks = ls.length, i=0; i<numLinks; i++) {
         ls[i].onclick= showSpinner;
     }
-    $('[href][onclick^="goto(event"]').click(showSpinner);
+    let elems = $('[href][onclick^="goto(event"]');
+    if ( elems ) {
+        elems.click(showSpinner);
+    }
     //if using the Back button to return to a page where the spinner was shown before navigating away, handle it
     window.addEventListener('pageshow', function (e) {
         //typescript would need: (performance.getEntriesByType("navigation")[0] as PerformanceNavigationTiming).type
