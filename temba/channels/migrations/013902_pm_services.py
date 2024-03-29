4572aecafdef8fd75167ed384f7b21980e27d02c
Ryan Fischbach <ryan.fischbach@twosixtech.com>
2024-03-29 16:05:32 -0700

    db: channels: no uuid gen trigger, must do ourselves.

--- temba/channels/migrations/013902_pm_services.py
+++ temba/channels/migrations/013902_pm_services.py
@@ -1,4 +1,5 @@
 from copy import deepcopy
+from uuid import uuid4
 
 from django.db import migrations
 
@@ -46,7 +47,7 @@ def create_pm_services(apps, schema_editor):  # pragma: no cover
             #make new channel and get its ID in parent_id
             parent_channel = deepcopy(channel)
             parent_channel.id = None
-            parent_channel.uuid = None
+            parent_channel.uuid = uuid4()
             parent_channel.schemes = parent_schemes
             parent_channel.name = parent_channel.name[:parent_channel.name.rfind('[')]
             parent_channel.name += '['+parent_scheme+']'
