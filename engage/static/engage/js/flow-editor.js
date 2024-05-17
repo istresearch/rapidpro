be5541bc09efc54a740b3f3fa7868eeb3772cfa7
Ryan Fischbach <ryan.fischbach@istresearch.com>
2024-05-16 17:29:47 -0700

    floweditor: add pan/zoom feature.

--- engage/static/engage/js/flow-editor.js
+++ engage/static/engage/js/flow-editor.js
@@ -1,59 +1,58 @@
-
-function initPanZoom( elDesktop ) {
-    const theEl = elDesktop;
-    const bZoomEnabled = false;
-    const $panzoom = Panzoom(theEl, {
+function initPanzoom() {
+    // called inside the haml file where used when flow editor finishes loading (important!)
+    let elDesktop;
+    let flowEditorPanzoom;
+    const configPanZoom = {
         minScale: 0.5,
         maxScale: 2,
         step: 0.05,
-        disableZoom: !bZoomEnabled, //until floweditor supports grabbing the scale from this obj
-        disablePan: true,
+        disableZoom: false,
+        disablePan: false,
         cursor: "",
-        canvas: true,
-        exclude: [document.getElementById('rp-flow-editor')],
-        excludeClass: ['jtk-draggable', 'pop_wrapper'],
-    });
+    };
 
-    $(theEl).parent().on('mousewheel.focal', (e) => {
-        //e.preventDefault();
-        let deltaY = e.deltaY || e.originalEvent.wheelDeltaY || (-e.originalEvent.deltaY);
-        let deltaX = e.deltaX || e.originalEvent.wheelDeltaX || (-e.originalEvent.deltaX);
-        if ( !(e.ctrlKey || e.originalEvent.ctrlKey) && bZoomEnabled && deltaY ) {
-            let bZoomOut = deltaY ? deltaY < 0 : e.originalEvent.deltaY > 0;
-            let zoomOpts = {
-               animate: true,
-               exponential: false,
-            };
-            if ( bZoomOut ) {
-                $panzoom.zoomOut(zoomOpts);
-            } else {
-                $panzoom.zoomIn(zoomOpts);
-            }
-        } else {
-            $panzoom.setOptions({
-                disablePan: false,
-            });
-            $panzoom.pan( deltaX / 2, deltaY / 2, {
-                animate: true,
-                relative: true,
-            });
-            $panzoom.setOptions({
-                disablePan: true,
-            });
-        }
-    });
-    return $panzoom;
-}
-let flowEditorPanzoom;
-window.addEventListener("DOMContentLoaded", function() {
-    const elDesktop = document.getElementById('rp-flow-editor');
-    flowEditorPanzoom = initPanZoom(elDesktop);
-
-    const menuitemReCenter = document.querySelector("#action-recenter");
-    if ( menuitemReCenter ) {
-        menuitemReCenter.addEventListener("click", (e) => {
-            flowEditorPanzoom.reset();;
+    function configurePanZoom(elDesktop) {
+        const theEl = elDesktop;
+        let config = Object.assign(configPanZoom, {
+            canvas: true,
+            exclude: Array.from(document.querySelectorAll('div[class^=CanvasDraggable]')),
         });
+        let zoomOpts = {
+            animate: true,
+            exponential: false,
+        };
+        let panOpts = {
+            animate: true,
+            relative: true,
+        };
+        const $panzoom = Panzoom(theEl, config);
+
+        $(theEl).parent().on('mousewheel.focal', (e) => {
+            let deltaY = e.deltaY || e.originalEvent.wheelDeltaY || (-e.originalEvent.deltaY);
+            let deltaX = e.deltaX || e.originalEvent.wheelDeltaX || (-e.originalEvent.deltaX);
+            if ( !config.disableZoom && (e.ctrlKey || e.originalEvent.ctrlKey) && deltaY ) {
+                e.preventDefault();
+                let bZoomOut = deltaY ? deltaY < 0 : e.originalEvent.deltaY > 0;
+                if ( bZoomOut ) {
+                    $panzoom.zoomOut(zoomOpts);
+                } else {
+                    $panzoom.zoomIn(zoomOpts);
+                }
+            } else if ( !config.disablePan && deltaX ) {
+                $panzoom.pan(deltaX/2, deltaY/2, panOpts);
+            }
+        });
+
+        return $panzoom;
     }
 
-});
+    elDesktop = document.getElementById('canvas-container');
+    flowEditorPanzoom = configurePanZoom(elDesktop);
+
+    const menuitemReCenter = document.querySelector("#action-recenter");
+    if ( menuitemReCenter && flowEditorPanzoom ) {
+        menuitemReCenter.addEventListener("click", (e) => {
+            flowEditorPanzoom.reset();
+        });
+    }
+}
